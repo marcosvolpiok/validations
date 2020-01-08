64d4d6cc205832484ce5d55391b8203bb8445952
Marcos Tomas Volpi <MVolpi@sofrecom.com.ar>
2020-01-08 15:39:36 -0300

    Add another class

---
+++ src/AnotherClass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PabloFerrari\TestPackage;
+
+class AnotherClass
+{
+    public $message;
+
+    public function __construct()
+    {
+        $this->message = "Version 3";
+    }
+}
