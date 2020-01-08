fbe9ec4e9123efeefbed6b8693a1f6f4e339567e
Pablo Ferrari <pferrari@sofrecom.com.ar>
2020-01-08 13:03:11 -0300

    first commit

---
+++ src/TestPackage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PabloFerrari\TestPackage;
+
+class TestPackage {
+
+    public $message;
+    
+    public function __construct() {
+        $this->message = "It's work!";
+    }
+
+}
+
+?>
